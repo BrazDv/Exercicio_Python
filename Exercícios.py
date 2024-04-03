81c5a5aa15846d0a1897e51dbb8b63940d3635c2
João <jbraz63@gmail.com>
2024-04-02 21:20:18 -0300

    Upando exercicio

--- Exercícios.py
+++ Exercícios.py
@@ -1,7 +1,5 @@
-# Solicitando o salário atual do colaborador
-salario = float(input("Digite o salário atual do colaborador: "))
+salario = float(input("Digite o salário do colaborador: "))
 
-# Definindo as faixas de aumento e seus respectivos percentuais
 if salario <= 280.00:
     percentual_aumento = 20
 elif salario <= 700.00:
@@ -11,17 +9,13 @@ elif salario <= 1500.00:
 else:
     percentual_aumento = 5
 
-# Calculando o valor do aumento
 aumento = salario * (percentual_aumento / 100)
 
-# Calculando o novo salário após o aumento
 novo_salario = salario + aumento
 
-# Calculando o valor do aumento real descontando a inflação
 taxa_inflacao = 0.038
 aumento_real = aumento - (aumento * taxa_inflacao)
 
-# Imprimindo os resultados na tela
 print("Salário antes do reajuste: R$", salario)
 print("Percentual de aumento aplicado: {}%".format(percentual_aumento))
 print("Valor do aumento: R$", aumento)
